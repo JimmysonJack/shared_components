712a4b44fc57221384f543a1427a812c7e3ceaec
chami <gilbert.chami@ega.go.tz>
2022-07-22 17:33:21 +0300

    TabsBar using index improvements

--- lib/src/tabs_bar/tabs_bar.dart
+++ lib/src/tabs_bar/tabs_bar.dart
@@ -1,13 +1,13 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:shared_component/src/tabs_bar/tabs.dart';
-import 'package:shared_component/src/tabs_bar/tabs_bar_store.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 
+/// Tab bar
 class TabsBar extends StatefulWidget {
-  const TabsBar({Key? key, required this.tabs}) : super(key: key);
+  TabsBar({Key? key, required this.tabs, this.selectedIndex = 0})
+      : super(key: key);
   final List<Tabs> tabs;
+  int selectedIndex;
+  Tabs? selectedTab;
 
   @override
   State<TabsBar> createState() => _TabsBarState();
@@ -16,79 +16,83 @@ class TabsBar extends StatefulWidget {
 class _TabsBarState extends State<TabsBar> {
   @override
   void initState() {
-    TabsBarStore.getInstance().setTabs(widget.tabs);
-
-    int selectedIndex = TabsBarStore.getInstance().tabsItems.indexWhere((element) => Modular.to.path.contains(element.url)) == -1
-        ? 0
-        : TabsBarStore.getInstance().tabsItems.indexWhere((secondElement) => Modular.to.path.contains(secondElement.url));
-
-    // TabsBarStore.getInstance().onSelected(
-    //     itemIndex: selectedIndex,
-    //     url: TabsBarStore.getInstance().tabsItems[selectedIndex].url);
+    if (widget.selectedIndex > widget.tabs.length - 1) {
+      widget.selectedTab = widget.tabs[0];
+    } else {
+      widget.selectedTab = widget.tabs[widget.selectedIndex];
+    }
     super.initState();
   }
 
-  int selectedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    double appBarSiz = AppBar().preferredSize.height;
-    BorderSide borderSide = BorderSide(color: Theme.of(context).primaryColor,width: 1);
-    BorderSide emptyBorder = const BorderSide(color: Colors.transparent,width: 0);
-    return Observer(
-      builder: (context) {
-        return Scaffold(
-            body: Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: appBarSiz * 0.7,
-                    child: ListView(
-                      padding: EdgeInsets.zero,
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      children: List.generate(TabsBarStore.getInstance().tabsItems.length, (index) => InkWell(
-                        onTap: (){
-                          setState(() {
-                            selectedIndex = index;
-                            TabsBarStore.getInstance().onSelected(
-                                itemIndex: selectedIndex,
-                                url: TabsBarStore.getInstance().tabsItems[selectedIndex].url);
-                          });
-                        },
-                        child: AnimatedContainer(
-                          duration: const Duration(milliseconds: 1000),
-                          curve: Curves.fastLinearToSlowEaseIn,
-                          alignment: Alignment.center,
-                          decoration:
-                          BoxDecoration(
-                              color: selectedIndex == index ? Theme.of(context).primaryColor : Colors.transparent,
-                              border: Border(top: borderSide, left: borderSide,right: TabsBarStore.getInstance().tabsItems.elementAt(TabsBarStore.getInstance().tabsItems.length -1) == TabsBarStore.getInstance().tabsItems.elementAt(index) ? borderSide : emptyBorder)),
-                          padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 10),
-                          child: Text(TabsBarStore.getInstance().tabsItems.elementAt(index).title,style: TextStyle(fontSize: Theme.of(context).textTheme.bodyLarge!.fontSize,color: selectedIndex == index ? Theme.of(context).cardColor : Theme.of(context).hintColor),),
-                        ),
-                      ),),
+    double appBarSize = AppBar().preferredSize.height;
+    BorderSide borderSide =
+        BorderSide(color: Theme.of(context).primaryColor, width: 1);
+    BorderSide emptyBorder =
+        const BorderSide(color: Colors.transparent, width: 0);
+
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: appBarSize * 0.7,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              children: List.generate(
+                widget.tabs.length,
+                (index) => InkWell(
+                  onTap: () {
+                    setState(() {
+                      widget.selectedTab = widget.tabs[index];
+                      widget.selectedIndex = index;
+                    });
+                  },
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 1000),
+                    curve: Curves.fastLinearToSlowEaseIn,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: widget.selectedIndex == index
+                            ? Theme.of(context).primaryColor
+                            : Colors.transparent,
+                        border: Border(
+                            top: borderSide,
+                            left: borderSide,
+                            right:
+                                widget.tabs.elementAt(widget.tabs.length - 1) ==
+                                        widget.tabs.elementAt(index)
+                                    ? borderSide
+                                    : emptyBorder)),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 10),
+                    child: Text(
+                      widget.tabs.elementAt(index).title,
+                      style: TextStyle(
+                          fontSize:
+                              Theme.of(context).textTheme.bodyLarge!.fontSize,
+                          color: widget.selectedIndex == index
+                              ? Theme.of(context).cardColor
+                              : Theme.of(context).hintColor),
                     ),
                   ),
-                  Expanded(
-                      flex: 12,
-                      child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                            border: Border(top: borderSide)
-                        ),
-                        child: const RouterOutlet(),
-                      )
-                  )
-                ],
+                ),
               ),
-            )
-        );
-      }
-    );
+            ),
+          ),
+          Expanded(
+              flex: 12,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(border: Border(top: borderSide)),
+                child: widget.selectedTab?.page,
+              ))
+        ],
+      ),
+    ));
   }
 }
-
-
-
